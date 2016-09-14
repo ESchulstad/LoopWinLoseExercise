ca0ec38d0a00f3d32bad156409824b0ed44e1d96
ESchulstad <eschulstad@gmail.com>
2016-09-13 20:21:32 -0400

    Initial Commit

---
+++ LoopWinLoseExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopWinLoseExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer.");
+            int num = int.Parse(Console.ReadLine());
+
+            do
+            {
+                if (num % 3 == 0)
+                {
+                    break;
+                }
+                Console.WriteLine("You Lost.");
+                Console.WriteLine("Please enter another integer.");
+                num = int.Parse(Console.ReadLine());
+
+            } while (true);
+
+            Console.WriteLine("You Won!");
+
+        }
+    }
+}
